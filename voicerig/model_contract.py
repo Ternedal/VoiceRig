d87b6b86d166dc652535d6710f546e240e1a3fda
Ternedal <a.nielbaek@gmail.com>
2026-08-19 18:07:25 +0200

    Record verified pyannote package version in model contract

--- voicerig/model_contract.py
+++ voicerig/model_contract.py
@@ -3,5 +3,6 @@
 CHATTERBOX_ENGINE = "chatterbox-multilingual"
 CHATTERBOX_MODEL = "v3"
 CHATTERBOX_SOURCE_REVISION = "5de7a54aa4e5e2baadb0182dde554908b48b85c2"
+PYANNOTE_PACKAGE_VERSION = "4.0.7"
 PYANNOTE_MODEL_ID = "pyannote/speaker-diarization-community-1"
 MODEL_READINESS_SCHEMA = 1

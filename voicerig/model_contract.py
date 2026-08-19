3a148fe8eabe2c5a323156f8972ffdb64cb74a73
Ternedal <a.nielbaek@gmail.com>
2026-08-19 17:57:09 +0200

    Centralize VoiceRig model contract

---
+++ voicerig/model_contract.py
@@ -0,0 +1,7 @@
+"""Pinned ML/runtime identifiers that define VoiceRig v1's voice contract."""
+
+CHATTERBOX_ENGINE = "chatterbox-multilingual"
+CHATTERBOX_MODEL = "v3"
+CHATTERBOX_SOURCE_REVISION = "5de7a54aa4e5e2baadb0182dde554908b48b85c2"
+PYANNOTE_MODEL_ID = "pyannote/speaker-diarization-community-1"
+MODEL_READINESS_SCHEMA = 1
